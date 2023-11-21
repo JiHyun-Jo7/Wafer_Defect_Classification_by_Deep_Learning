6cb733b258ddc8afed236c8192f69308490e5c86
JiHyun-Jo7 <jihn2824@gmail.com>
2023-11-21 16:36:32 +0900

    불필요한 데이터 삭제 필요 (Normal)

--- job02_transform_data.py
+++ job02_transform_data.py
@@ -1,46 +1,55 @@
 import numpy as np
 import pandas as pd
-from pandas import DataFrame, Series
-from scipy.io import mmwrite, mmread
 import pickle
 import matplotlib.pyplot as plt
-
-# loading libraries
 import skimage
 from skimage import measure
-from skimage.transform import radon
-from skimage.transform import probabilistic_hough_line
-from skimage import measure
-from scipy import interpolate
-from scipy import stats
+from skimage.transform import radon, probabilistic_hough_line
+from scipy.io import mmwrite, mmread
+from scipy import interpolate, stats
+import sys
+import warnings
 
-# 0 147431
-# 1 4294
-# 2 555
-# 3 5189
-# 4 9680
-# 5 3593
-# 6 866
-# 7 1193
-# 8 149
+warnings.filterwarnings("ignore")  # 경고문 출력 제거
+np.set_printoptions(threshold=sys.maxsize)  # 배열 전체 출력
+pd.set_option('display.max_columns', None)
+
+# loading libraries
+# 0. 'Normal': 147431, 1. Center': 4294, 2. 'Donut': 555, 3. 'Edge-Loc': 5189,
+# 4. 'Edge-Ring': 9680, 5. 'Loc': 3593, 6. 'Random': 866, 7. 'Scratch': 1193, 8. 'Near-full': 149
 
 # 카테고리별 데이터양 맞춰주기
-df_clean=pd.read_pickle("./datasets/LSWMD_clean_data.pickle")
+df_clean = pd.read_pickle("./datasets/LSWMD_clean_data.pickle")
 pd.set_option('display.max_columns', None)
 df_clean.info()
 # df_clean.sort_values('failureNum', inplace=True)
 df_clean.set_index('waferMap', inplace=True)
 df_clean.reset_index(inplace=True)
-print(df_clean)
+print('df_clean')
+# print(df_clean.head())
+test = df_clean.iloc[34839]
+print(test)
+
+exit()
+# index_Num = df_clean.index[df_clean['failureType'] == 'Near-full']
+# print(index_Num)
+
+x = [0, 43, 6492, 35, 97, 19, 541, 130, 814]
+labels2 = ['Normal', 'Center', 'Donut', 'Edge-Loc', 'Edge-Ring', 'Loc', 'Random', 'Scratch', 'Near-full']
 
 # 구역별 밀도 설정
+
+
 def cal_den(x):
     return 100 * (np.sum(x == 2) / np.size(x))
+
+
 def find_regions(x):
     rows = np.size(x, axis=0)
     cols = np.size(x, axis=1)
-    ind1 = np.arange(0, rows, rows // 5)
-    ind2 = np.arange(0, cols, cols // 5)
+
+    ind1 = np.arange(0, rows, rows//5)
+    ind2 = np.arange(0, cols, cols//5)
 
     reg1 = x[ind1[0]:ind1[1], :]
     reg3 = x[ind1[4]:, :]
@@ -62,19 +71,23 @@ def find_regions(x):
                    cal_den(reg7), cal_den(reg8), cal_den(reg9), cal_den(reg10), cal_den(reg11), cal_den(reg12),
                    cal_den(reg13)]
     return fea_reg_den
+
+
 df_clean['fea_reg'] = df_clean.waferMap.apply(find_regions)
+
 
 # 보간법으로 waferMap 사이즈 맞추기
 def cubic_inter_mean(img):
     theta = np.linspace(0., 180., max(img.shape), endpoint=False)
     sinogram = radon(img, theta=theta)
-    xMean_Row = np.mean(sinogram, axis = 1)
+    xMean_Row = np.mean(sinogram, axis=1)
     x = np.linspace(1, xMean_Row.size, xMean_Row.size)
     y = xMean_Row
-    f = interpolate.interp1d(x, y, kind = 'cubic')
+    f = interpolate.interp1d(x, y, kind='cubic')
     xnew = np.linspace(1, xMean_Row.size, 20)
-    ynew = f(xnew)/100   # use interpolation function returned by `interp1d`
+    ynew = f(xnew) / 100  # use interpolation function returned by `interp1d`
     return ynew
+
 
 def cubic_inter_std(img):
     theta = np.linspace(0., 180., max(img.shape), endpoint=False)
@@ -82,11 +95,14 @@ def cubic_inter_std(img):
     xStd_Row = np.std(sinogram, axis=1)
     x = np.linspace(1, xStd_Row.size, xStd_Row.size)
     y = xStd_Row
-    f = interpolate.interp1d(x, y, kind = 'cubic')
+    f = interpolate.interp1d(x, y, kind='cubic')
     xnew = np.linspace(1, xStd_Row.size, 20)
-    ynew = f(xnew)/100   # use interpolation function returned by `interp1d`
+    ynew = f(xnew) / 100  # use interpolation function returned by `interp1d`
     return ynew
 
-df_clean['fea_cub_mean'] =df_clean.waferMap.apply(cubic_inter_mean)
-df_clean['fea_cub_std'] =df_clean.waferMap.apply(cubic_inter_std)
+
+df_clean['fea_cub_mean'] = df_clean.waferMap.apply(cubic_inter_mean)
+df_clean['fea_cub_std'] = df_clean.waferMap.apply(cubic_inter_std)
+
+
 
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import sys
 warnings.filterwarnings("ignore")  # 경고문 출력 제거
 np.set_printoptions(threshold=sys.maxsize)  # 배열 전체 출력
 pd.set_option('display.max_columns', None)
-df = pd.read_pickle("./input/LSWMD.pkl")  # 피클에 있는 데이터 프레임 read
+df = pd.read_pickle("./datasets/LSWMD.pkl")  # 피클에 있는 데이터 프레임 read
 # df.info()
 # waferMap          웨이퍼 사진 (0 : 공백 | 1 : 웨이퍼 | 2 : 결함)
 # dieSize           다이싱 사이즈
@@ -32,7 +32,32 @@ def find_dim(x):
 
 # dataframe 에 wafer size 추가
 df['waferMapDim'] = df.waferMap.apply(find_dim)
-# print(max(df.waferMapDim), min(df.waferMapDim))  # df.waferMapDim 최대 최소값 확인
+print(max(df.waferMapDim), min(df.waferMapDim))  # df.waferMapDim 최대 최소값 확인
+sorted_list_X = sorted(df.waferMapDim, key=lambda x: x[0], reverse=False)
+sorted_list_Y = sorted(df.waferMapDim, key=lambda x: x[1], reverse=False)
+topX_values = sorted_list_X[:50]
+topY_values = sorted_list_Y[:50]
+print(topX_values)
+print(topY_values)
+
+index_Num = df.index[df['waferMapDim'] == (10, 150)]
+# print(index_Num[:100])
+idx = [266785, 266786, 266787, 266788, 266789, 266790, 266791, 266792,
+        266793, 266794, 266795, 266796, 266797, 266798, 266799, 266800,
+        266801, 266802, 266803, 266804, 266805, 266806, 266807, 266808,
+        266809, 266835, 266836, 266837, 266838, 266839, 266840, 266841,
+        266842, 266843, 266844, 266845, 266846, 266847, 266848, 266849,
+        266850, 266851, 266852, 266853, 266854, 266855, 266856, 266857,
+        266858, 266859, 266860, 266861, 266862, 266863, 266864, 266865,
+        266866, 266867, 266868, 266869, 266870, 266871, 266872, 266873,
+        266874, 266875, 266876, 266877, 266878, 266879, 266880, 266881,
+        266882, 266883, 266884, 277186, 277187, 277188, 277189, 277190,
+        277191, 277192, 277193, 277194, 277195, 277196, 277197, 277198,
+        277199, 277200, 277201, 277202, 277203, 277204, 277205, 277206,
+        277207, 277208, 277209, 277210]
+test = df['failureType'].iloc[idx]
+print(test)
+exit()
 
 # failureType Data Labeling
 
@@ -60,17 +85,18 @@ df_nonpattern = df_nonpattern.reset_index()
 # 분류 없음
 df_Nan = df[(df['failureNum'] == 9)]
 df_Nan = df_Nan.reset_index()
-df_Nan.info()
-print(df_Nan)
+
+index_Num = df_nonpattern.index[df_nonpattern['waferMapDim'] == (6, 21)]
+print(index_Num)
 
 fig, ax = plt.subplots(nrows=10, ncols=10, figsize=(20, 20))
 ax = ax.ravel(order='C')
 
-for i in range(100):
-    img = df_Nan.waferMap[i]
+for i in range(0, 100):
+    img = df_nonpattern.waferMap[i+17283]
     ax[i].imshow(img)
-    ax[i].set_title(df_Nan.failureType[i][0][0], fontsize=10)
-    ax[i].set_xlabel(df_Nan.index[i], fontsize=8)
+    ax[i].set_title(df_nonpattern.failureType[i][0][0], fontsize=10)
+    ax[i].set_xlabel(df_nonpattern.index[i], fontsize=8)
     ax[i].set_xticks([])
     ax[i].set_yticks([])
 plt.tight_layout()
