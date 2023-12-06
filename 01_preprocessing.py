5c759057d3c0aded66db72568aad6ebfda11897d
JiHyun-Jo7 <jihn2824@gmail.com>
2023-12-06 17:45:03 +0900

    Check Missing Value & Make Pickle

--- 01_preprocessing.py
+++ 01_preprocessing.py
@@ -1,5 +1,3 @@
-from sklearn.preprocessing import StandardScaler
-from matplotlib import gridspec
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,8 +10,8 @@ warnings.filterwarnings("ignore")  # 경고문 출력 제거
 np.set_printoptions(threshold=sys.maxsize)  # 배열 전체 출력
 pd.set_option('display.max_columns', None)
 df = pd.read_pickle("./datasets/LSWMD.pkl")  # 피클에 있는 데이터 프레임 read
-
 # df.info()
+print("debug01")
 # waferMap          웨이퍼 사진 (0 : 공백 | 1 : 웨이퍼 | 2 : 결함)
 # dieSize           다이싱 사이즈
 # lotName           웨이퍼 1묶음의 번호
@@ -24,33 +22,35 @@ df = pd.read_pickle("./datasets/LSWMD.pkl")  # 피클에 있는 데이터 프레
 # 사용하지 않는 데이터 제거
 df = df.drop(['waferIndex', 'dieSize', 'lotName'], axis=1)
 # df.info()
-
-
+print("debug02")
 # failureType Data Labeling
-df.loc[df['failureType'].str.len() == 0, "failureType"] = np.nan
+# df.loc[df['failureType'].str.len() == 0, "failureType"] = np.nan
 # df['failureType'] = df['failureType'].fillna("Nan")
-
-df['failureNum'] = df.failureType
-df['trainTestNum'] = df.trianTestLabel
-
-mapping_type = {'Center': 1, 'Donut': 2, 'Edge-Loc': 3, 'Edge-Ring': 4, 'Loc': 5, 'Random': 6, 'Scratch': 7,
-                'Near-full': 8, 'none': 0}  # , "Nan": 9
-mapping_traintest = {'Training': 0, 'Test': 1}
-df = df.replace({'failureNum': mapping_type, 'trainTestNum': mapping_traintest})
-
-df = df.drop(['trianTestLabel'], axis=1)
 
 
 # 'none' -> 'Normal'
 def replace_value(defect):
-    # 만약 cell_value가 [['none']]이면 [['Normal']]로 변경
-    return [['Normal']] if defect == [['none']] else defect
+    if defect == [['none']]:
+        defect = [['Normal']]
+    else: pass
+    return defect
 
 
-# 'failureType' 열에 대해 사용자 정의 함수를 적용하여 값 변경
 df['failureType'] = df['failureType'].apply(replace_value)
 
 print(df['failureType'].value_counts())
+
+df['failureNum'] = df.failureType
+df['trainTestNum'] = df.trianTestLabel
+
+mapping_type = {'Normal': 0, 'Center': 1, 'Donut': 2, 'Edge-Loc': 3, 'Edge-Ring': 4, 'Loc': 5, 'Random': 6, 'Scratch': 7,
+                'Near-full': 8}  # , "Nan": 9
+mapping_traintest = {'Training': 0, 'Test': 1}
+df = df.replace({'failureNum': mapping_type, 'trainTestNum': mapping_traintest})
+print("debug03")
+df = df.drop(['trianTestLabel'], axis=1)
+print("debug04")
+
 
 # 분류된 데이터
 df_withlabel = df[(df['failureNum'] >= 0) & (df['failureNum'] <= 8)]
@@ -65,12 +65,10 @@ df_nonpattern = df_nonpattern.reset_index()
 # df_Nan = df[(df['failureNum'] == 9)]
 # df_Nan = df_Nan.reset_index()
 
-
 def find_dim(x):
     dim0 = np.size(x, axis=0)
     dim1 = np.size(x, axis=1)
     return dim0, dim1
-
 
 # dataframe 에 wafer size 추가
 df['waferMapDim'] = df.waferMap.apply(find_dim)
@@ -96,6 +94,10 @@ print('minY:', topY_values)
 # print('less X/Y 50%:', less50_X)
 # print('less Y/X 50%:', less50_Y)
 
+index_Num_wl = df_withlabel.index[
+    (df_withlabel['waferMapDim'] == (15, 3)) | (df_withlabel['waferMapDim'] == (18, 4)) |
+    (df_withlabel['waferMapDim'] == (18, 44)) | (df_withlabel['waferMapDim'] == (24, 13)) |
+    (df_withlabel['waferMapDim'] == (27, 15)) | (df_withlabel['waferMapDim'] == (24, 18))]
 index_Num_np = df_nonpattern.index[
     (df_nonpattern['waferMapDim'] == (15, 3)) | (df_nonpattern['waferMapDim'] == (18, 4)) |
     (df_nonpattern['waferMapDim'] == (18, 44)) | (df_nonpattern['waferMapDim'] == (24, 13)) |
@@ -107,9 +109,14 @@ index_Num_df = df.index[(df['waferMapDim'] == (15, 3)) | (df['waferMapDim'] == (
 index_list_np = index_Num_np.tolist()
 index_list_df = index_Num_df.tolist()
 print(len(index_list_np), len(index_list_df))
+
+df_withlabel = df_withlabel[~df_withlabel.index.isin(index_list_np)]
 df_nonpattern = df_nonpattern[~df_nonpattern.index.isin(index_list_np)]
 df = df[~df.index.isin(index_list_df)]
+
+df_withlabel = df_withlabel.reset_index()
 df_nonpattern = df_nonpattern.reset_index()
+df = df.reset_index()
 
 
 fig, ax = plt.subplots(nrows=4, ncols=5, figsize=(10, 10))
