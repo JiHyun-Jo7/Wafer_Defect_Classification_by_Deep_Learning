f97216fc9f317ba728c8e5dda01bbef501a0f696
JiHyun-Jo7 <jihn2824@gmail.com>
2023-12-14 10:19:31 +0900

    model test

--- 03_CNN.py
+++ 03_CNN.py
@@ -5,6 +5,7 @@ from PIL import Image
 from keras.utils import to_categorical
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
+from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import sys
@@ -56,8 +57,8 @@ print('\nresize waferMap :')
 # print(filtered_df.resized_waferMap[0])
 
 # 데이터를 훈련 및 테스트 세트로 분할
-# X_train, X_test, Y_train, Y_test = train_test_split(filtered_df['resized_waferMap'], filtered_df['failureNum'], test_size=0.2, random_state=42)
-X_train, X_test, Y_train, Y_test = train_test_split(np.array(filtered_df['resized_waferMap'].tolist()), filtered_df['failureNum'], test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(np.array(filtered_df['resized_waferMap'].tolist()),
+                                                    filtered_df['failureNum'], test_size=0.2, random_state=42)
 
 # 훈련 데이터 및 레이블 확인
 print("\n훈련 데이터 형태:")
@@ -73,11 +74,11 @@ plt.imshow(X_train[my_sample], cmap='gray')  # cmap = 'gray': 흑백 처리
 plt.show()
 
 # 레이블을 원-핫 인코딩
-Y_train = to_categorical(Y_train)
-Y_test = to_categorical(Y_test)
+y_train = to_categorical(Y_train)
+y_test = to_categorical(Y_test)
 
 # 레이블 확인
-original_label = np.argmax(Y_train[my_sample])
+original_label = np.argmax(y_train[my_sample])
 print("\n원래 레이블:", original_label)
 
 # 매핑된 레이블 확인
@@ -86,7 +87,7 @@ print("\n매핑된 레이블:", mapped_label)
 
 # 원-핫 인코딩된 레이블 확인
 print("\n원-핫 인코딩된 레이블:")
-print(Y_train[my_sample])
+print(y_train[my_sample])
 
 # 'labels' 리스트를 사용하여 레이블 출력
 labels = ['Normal', 'Center', 'Donut', 'Edge-Loc', 'Edge-Ring', 'Loc', 'Random', 'Scratch', 'Near-full']
@@ -94,8 +95,8 @@ print("\ntag:", labels[original_label])
 # print(X_train.iloc[my_sample])  # 픽셀 값. 0~255 밝을수록 값이 커짐
 print('type: ', type(X_train[my_sample]))
 
-x_train = X_train / 2    # max(x_train) = 2
-x_test = X_test / 2      # max(x_test) = 2
+x_train = X_train / 2  # max(x_train) = 2
+x_test = X_test / 2  # max(x_test) = 2
 
 x_dim, y_dim = target_size
 x_train = x_train.reshape(len(X_train), x_dim, y_dim, 1)
@@ -105,3 +106,38 @@ print('\nx_train.shape:')
 print(x_train.shape)
 print("\nx_test.shape:")
 print(x_test.shape)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu', padding='same', input_shape=(x_dim, y_dim, 1)))
+model.add(MaxPool2D(padding='same', pool_size=(2, 2)))
+model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu'))
+model.add(MaxPool2D(padding='same', pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dropout(0.2))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(9, activation='softmax'))  # 예측 값 (카테고리 수)
+model.summary()
+
+opt = Adam(learning_rate=0.01)
+model.compile(opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+fit_hist = model.fit(x_train, y_train, batch_size=128,
+                     epochs=20, validation_split=0.2, verbose=1)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Final test set accuracy', score[1])
+
+plt.plot(fit_hist.history['accuracy'])
+plt.plot(fit_hist.history['val_accuracy'])
+plt.show()
+
+my_sample = np.random.randint(10000)
+plt.imshow(X_test[my_sample], cmap='gray')
+
+print(labels[Y_test[my_sample]])
+pred = model.predict(x_test[my_sample].reshape(-1, x_dim, y_dim, 1))
+
+print("pred: ", pred)  # 0~9까지 각 숫자일 확률 출력
+print("argmax: ", labels[np.argmax(pred)])  # numpy.argmax() 주어진 NumPy 배열에서 가장 높은 값을 가진 값의 인덱스를 반환
