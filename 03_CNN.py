2b62a409b54385ffa76ab0658cf88713e7f49ec8
JiHyun-Jo7 <jihn2824@gmail.com>
2023-12-14 09:45:48 +0900

    test

--- 03_CNN.py
+++ 03_CNN.py
@@ -53,10 +53,11 @@ print(target_size)
 # 'resized_waferMap' 열에 리사이즈된 데이터 추가
 filtered_df['resized_waferMap'] = filtered_df['waferMap'].apply(lambda x: resize_wafer_map(x, target_size))
 print('\nresize waferMap :')
-print(filtered_df.resized_waferMap[0])
+# print(filtered_df.resized_waferMap[0])
 
 # 데이터를 훈련 및 테스트 세트로 분할
-X_train, X_test, Y_train, Y_test = train_test_split(filtered_df['resized_waferMap'], filtered_df['failureNum'], test_size=0.2, random_state=42)
+# X_train, X_test, Y_train, Y_test = train_test_split(filtered_df['resized_waferMap'], filtered_df['failureNum'], test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(np.array(filtered_df['resized_waferMap'].tolist()), filtered_df['failureNum'], test_size=0.2, random_state=42)
 
 # 훈련 데이터 및 레이블 확인
 print("\n훈련 데이터 형태:")
@@ -68,40 +69,39 @@ print(X_test.shape, Y_test.shape)
 
 # 이미지 플로팅 및 레이블 확인
 my_sample = np.random.randint(len(X_train))
-plt.imshow(X_train.iloc[my_sample], cmap='gray')  # cmap = 'gray': 흑백 처리
+plt.imshow(X_train[my_sample], cmap='gray')  # cmap = 'gray': 흑백 처리
 plt.show()
 
+# 레이블을 원-핫 인코딩
+Y_train = to_categorical(Y_train)
+Y_test = to_categorical(Y_test)
+
 # 레이블 확인
-original_label = Y_train.iloc[my_sample]
+original_label = np.argmax(Y_train[my_sample])
 print("\n원래 레이블:", original_label)
 
 # 매핑된 레이블 확인
 mapped_label = label_mapping[original_label]
 print("\n매핑된 레이블:", mapped_label)
 
-# 레이블을 원-핫 인코딩
-Y_train = to_categorical(Y_train)
-Y_test = to_categorical(Y_test)
-
 # 원-핫 인코딩된 레이블 확인
 print("\n원-핫 인코딩된 레이블:")
 print(Y_train[my_sample])
 
-labels = ['Normal', 'Center', 'Donut', 'Edge-Loc', 'Edge-Ring', 'Loc', 'Random', 'Scratch', 'Near-full']
-
 # 'labels' 리스트를 사용하여 레이블 출력
+labels = ['Normal', 'Center', 'Donut', 'Edge-Loc', 'Edge-Ring', 'Loc', 'Random', 'Scratch', 'Near-full']
 print("\ntag:", labels[original_label])
-print(X_train.iloc[my_sample])  # 픽셀 값. 0~255 밝을수록 값이 커짐
-print('type: ', type(X_train.iloc[my_sample]))
+# print(X_train.iloc[my_sample])  # 픽셀 값. 0~255 밝을수록 값이 커짐
+print('type: ', type(X_train[my_sample]))
 
-x_train = X_train.values / 2    # max(x_train) = 2
-x_test = X_test.values / 2      # max(x_test) = 2
+x_train = X_train / 2    # max(x_train) = 2
+x_test = X_test / 2      # max(x_test) = 2
+
+x_dim, y_dim = target_size
+x_train = x_train.reshape(len(X_train), x_dim, y_dim, 1)
+x_test = x_test.reshape(-1, x_dim, y_dim, 1)
 
 print('\nx_train.shape:')
 print(x_train.shape)
 print("\nx_test.shape:")
 print(x_test.shape)
-
-x_dim, y_dim = target_size
-x_train = x_train.reshape(len(X_train), x_dim, y_dim, 1)
-x_test = x_test.reshape(-1, x_dim, y_dim, 1)
