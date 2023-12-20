aad2f19ec18ea7b1751e31a2b1336a3606fb3ef0
kingscrosss <145517821+kingscrosss@users.noreply.github.com>
2023-12-20 15:39:33 +0900

    데이터 전처리(노말 데이터 수량 조절 및 훈련,테스트 데이터 분리)

--- job02_CNN.py
+++ job02_CNN.py
@@ -1,17 +1,24 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Activation, Conv2D, MaxPool2D, Dropout  # CNN의 핵심: Conv2D, MaxPool2D
+from PIL import Image
+from keras.utils import to_categorical
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
 from tensorflow.keras.optimizers import Adam
-from keras.utils import np_utils        # from tensorflow.python.keras.utils import np_utils 로 바꼈단다...
-from tensorflow.keras import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from statistics import median
+import time
+import sys
+import warnings
 
-training_data=pd.read_pickle('./datasets/LSWMD_final_data.pickle')
+
+training_data=pd.read_pickle('./datasets/LSWMD_Train.pickle')
 training_data.info()
 
-target = [] #############
-(X_train, Y_train), (X_test, Y_test) = train_test_split(training_data, target, test_size = 0.2)
-print(X_train.shape, Y_train.shape)
-print(X_test.shape, Y_test.shape)
+# target = [] #############
+# (X_train, Y_train), (X_test, Y_test) = train_test_split(training_data, target, test_size = 0.2)
+# print(X_train.shape, Y_train.shape)
+# print(X_test.shape, Y_test.shape)
+
