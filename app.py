a5ae49b348dcd5977f9d4c2a63a4f0b3a73d0075
JiHyun-Jo7 <jihn2824@gmail.com>
2024-01-26 17:21:53 +0900

    app_modify

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from PyQt5.QtWidgets import *
 import numpy as np
 from tensorflow.keras.models import load_model
 from PyQt5 import uic
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvas as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
 import sys, warnings
 import pandas as pd
 
@@ -11,8 +16,10 @@ warnings.filterwarnings("ignore")  # 경고문 출력 제거
 np.set_printoptions(threshold=sys.maxsize)  # 배열 전체 출력
 pd.set_option('display.max_columns', None)
 
-form_window = uic.loadUiType('./classification.ui')[0]
+form_window = uic.loadUiType('./classification_test.ui')[0]
 
+# X_train, X_test, x_train, x_test, Y_train, Y_test, y_train, y_test, label_mapping = np.load(
+#                 './datasets/train_test_data_0.953.pkl', allow_pickle=True)
 
 class Exam(QWidget, form_window):  # 클래스 생성
     def __init__(self):
@@ -20,129 +27,160 @@ class Exam(QWidget, form_window):  # 클래스 생성
         self.setupUi(self)
         self.setFixedWidth(450)
         self.setFixedHeight(700)
-        self.btn_open.clicked.connect(self.btn_open_clicked_slot)
-        self.btn_search.clicked.connect(self.btn_search_clicked)
-        self.cb.activated[str].connect(self.Changed_Str)
-        # self.le.textChanged[str].connect(self.Changed_Str)
-        model_path = './models/CNN_0.927.h5'
+
+        # Plot
+        self.fig = plt.Figure()
+        self.canvas = FigureCanvas(self.fig)
+
+        # 모델 불러오기
+        model_path = '../test/models/CNN_0.883.h5'
         self.model = load_model(model_path)
-        self.path = ('./Img_sample/gui.png', '')
-        pixmap = QPixmap(self.path[0])
-        self.lb_img.setPixmap(pixmap)
 
-    def btn_search_clicked(self, lb):
+        # 초기 화면 설정
+        self.lb_img.setText("Wafer Defect Classification Program\nEnter Category or Index Num")
+
+        # 함수 연결
+        self.btn_search.clicked.connect(self.Btn_search_clicked)
+        self.cb.activated[str].connect(self.Choose_category)
+        self.le.textChanged.connect(self.Le_changed)
+
+        # 변수 초기화
+        self.labels = ['Normal', 'Center', 'Donut', 'Edge-Loc', 'Edge-Ring', 'Loc', 'Random', 'Scratch', 'Near-Full']
+        self.label_mapping = {0: 'Normal', 1: 'Center', 2: 'Donut', 3: 'Edge-Loc', 4: 'Edge-Ring', 5: 'Loc',
+                              6: 'Random', 7: 'Scratch', 8: 'Near-full'}
+        self.X_train = None
+        self.X_test = None
+        self.x_train = None
+        self.x_test = None
+        self.Y_train = None
+        self.Y_test = None
+        self.y_train = None
+        self.y_test = None
+        self.label_mapping = None
+
+        self.lb_pred.setText('')
+        self.lb_label.setText('')
+
+    def Pickle_load(self):
+        self.X_train, self.X_test, self.x_train, self.x_test, self.Y_train, self.Y_test, self.y_train, self.y_test, self.label_mapping = np.load(
+            './datasets/train_test_data_0.953.pkl', allow_pickle=True)
+        self.Y_test = self.Y_test.reset_index(drop=True)
+
+
+    def Btn_search_clicked(self):
         try:
-            try:
-                print('debug:Change Str 01')
-                lb = int(lb)
-                self.Changed_Num(lb)
-            except: self.Changed_Str(lb)
-        except: print('error : btn_search_clicked')
-
-    def btn_open_clicked_slot(self):  # 버튼을 누르는 동안 반응하는 함수    (눌렀다 떼면 반응 -> released)
-        old_path = self.path
-        self.path = QFileDialog.getOpenFileName(self, 'Open file',
-                                                './Img_sample', 'Image Files(*.jpg;*png);; All Files(*.*)')
-        # 이미지 불러오기 (확장자 제한 : jpg, png, 모든 파일)
-        if self.path[0] == '':
-            self.path = old_path  # 불러오기를 취소할 경우 기존 사진으로 복원 (프로그램 꺼짐 방지)
-        print(self.path)
-        pixmap = QPixmap(self.path[0])  # 이미지 라벨을 선택한 사진으로 교체
-        self.lb_img.setPixmap(pixmap)
-
-        try:
-            img = Image.open(self.path[0])
-            self.classification(self.preprocessing(img))
-
-        except:
-            print('error : {}'.format(self.path[0]))
-
-    def Changed_Num(self, num):
-        try:
-            print('debug:Change Num 01')
-            df_train, df_test = np.load('./datasets/train_test_{}.pkl', allow_pickle=True)
-            X_test = df_test.waferMap
-
-            if 0 <= num < len(X_test):
-                print('debug:Change Num 02')
-                img_array = X_test[num]
-
-                height, width = img_array.shape
-                bytes_per_line = width
-                q_image = QImage(img_array.data, width, height, bytes_per_line, QImage.Format_Grayscale8)
-
-                pixmap = QPixmap.fromImage(q_image)
-
-                self.lb_img.setPixmap(pixmap)
+            if self.le.text().isdigit():
+                label = int(self.le.text())
+                self.Find_index(label)
+                print('debug : It is number')
             else:
-                print('debug:Change Num 02')
-                self.lb_img.setText("Wrong Index")
-        except: print('error : Change Num')
-        # df_train, df_test = np.load(
-        #     './datasets/train_test_{}.pkl', allow_pickle=True)
-        # X_test = df_test.waferMap
-        # if 0 <= int <= len(X_test):
-        #     test_img = X_test[int]  # 이미지 라벨을 선택한 사진으로 교체
-        #     self.lb_img.setPixmap(test_img)
-        # else:
-        #     self.lb_img.setText("Wrong Index")
+                label = str(self.le.text())
+                self.Find_category(label)
+                print('debug : It is str')
+        except Exception as e:
+            print('error:', e)
+            print('error : btn_search_clicked')
 
-    def Changed_Str(self, lb):
+    def Find_index(self, label):
         try:
-            labels = ['Normal', 'Center', 'Donut', 'Edge-Loc', 'Edge-Ring', 'Loc', 'Random', 'Scratch', 'Near-full']
+            self.Pickle_load()
+            # 존재하는 인덱스 값인 경우
+            if label < len(self.x_test):
+                print('test2')
+            # 인덱스 값이 없을때
+            else: print('Wrong Index Num')
 
-            if lb in labels:
-                print('debug:Change Str 02')
-                self.choice_ramdom_index(lb)
-                self.lb_img.setPixmap(self.random_waferMap)
-                self.classification(self.random_waferMap)
-            elif lb == 'category':
-                print('debug:Change Str 03')
+        except Exception as e:
+            print('error:', e)
+            print('error : Find_index')
+
+    def Find_category(self, label):
+        try:
+            self.Pickle_load()
+            if label.title() in self.labels:
+                print('Debug: Find_category')
+                # 해당 카테고리 중 랜덤 인덱스 하나 고르기
+                numeric_label = self.labels.index(label.title())
+                print('numeric_label: ', numeric_label)
+                selected_rows = self.Y_test[self.Y_test == numeric_label]
+                random_index = np.random.choice(selected_rows.index)
+                print('random_index: ', random_index)
+                # 이미지 플롯
+                self.Plot_wafer_img(random_index)
+                # 결함 종류 예측 및 정확도 출력
+                self.Predict(random_index)
+            elif label == 'Category':
+                # 다른 동작 대기
                 pass
             else:
-                print('debug:Change Str 04')
+                print('debug: Find_category - else')
                 self.lb_img.setText("Wrong Category")
-        except: print('error : Change Str')
 
-    def classification(self, img):
+        except Exception as e:
+            print('error:', e)
+            print('error : Fine_category')
+
+    def Choose_category(self, label):
         try:
-            print('debug:classification 01')
-            pred = self.model.predict(img)
-            predicted_prob = np.max(pred)
-            pred_str = f'Probability: {predicted_prob * 100:.2f}%'
-            self.lb_pred.setText(pred_str)
+            self.le.clear()
+            print(label)
+            self.Find_category(label)
+        except Exception as e:
+            print('error:', e)
+            print('error : Choose_combobox')
 
-            labels = ['Normal', 'Center', 'Donut', 'Edge-Loc', 'Edge-Ring', 'Loc', 'Random', 'Scratch', 'Near-full']
-            predicted_label = labels[np.argmax(pred)]
-            print('debug:classification 02')
-            self.lb_label.setText(f'Predicted Label: {predicted_label}')
-        except:
-            print('error : classification')
+    def Le_changed(self):
+        # 텍스트가 변경되면 ComboBox 초기화
+        self.cb.setCurrentIndex(0)
 
-    def choice_ramdom_index(self, label):
+    def Plot_wafer_img(self, index):
         try:
-            print('debug:choice_ramdom_index 01')
-            df_train, df_test = np.load('./datasets/train_test_{}.pkl', allow_pickle=True)
-            print('debug:choice_ramdom_index 02')
-            category = df_test[df_test['failureNum'] == label]
-            print('debug:choice_ramdom_index 03')
-            random_waferMap = np.random.choice(category['waferMap'])
-            print('debug:choice_ramdom_index 04')
+            self.lb_img.setText("")
+            self.Pickle_load()
+            true_label = self.labels[self.Y_test[index]]
 
-            return random_waferMap
-        except: print('error : choice_ramdom_index')
+            if not self.fig.get_axes():
+                ax = self.fig.add_subplot(111)
+            else:
+                ax = self.fig.get_axes()[0]
+                ax.clear()
 
-    def preprocessing(self, img):
+            ax.imshow(self.X_test[index], cmap=plt.cm.gray, vmin=0, vmax=2)
+
+            ax.set_title(f'True Label: {true_label}')
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.legend([])
+            self.canvas.draw()
+
+            q_img = QImage(self.canvas.buffer_rgba(), self.canvas.get_width_height()[0], self.canvas.get_width_height()[1],
+                           QImage.Format_ARGB32)
+            pixmap = QPixmap.fromImage(q_img)
+
+            # QPixmap을 QLabel에 설정하여 이미지 표시
+            self.lb_img.setPixmap(pixmap)
+
+            print(self.X_test[index])
+
+        except Exception as e:
+            print('error:', e)
+            print('error : plot_img')
+
+    def Predict(self, index):
         try:
-            print('debug:preprocessing 01')
-            img = img.convert('RGB')
-            data = np.asarray(img)
-            data = data / 255
-            data = data.resize((37, 37))
-            print(data)
-            data = data.reshape(-1, 37, 37, 1)
-            return data
-        except: print('error : preprocessing')
+            self.Pickle_load()
+            pred = self.model.predict(self.X_test[index].reshape(1, 37, 37, 1))
+            print(pred)
+            max_pred = np.max(pred)
+            max_percentage = max_pred * 100
+            self.lb_pred.setText(f"{max_percentage:.3f}%")
+
+            pred_label = self.labels[np.argmax(pred)]
+            print(pred_label)
+            self.lb_label.setText(pred_label)
+        except Exception as e:
+            print('error:', e)
+            print('error : Predict')
 
 
 if __name__ == '__main__':  # 메인
